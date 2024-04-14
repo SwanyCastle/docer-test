ebac0fc2bdffbcc132a887b72adc9246a29ad60b
SwanyCastle <seunghwan712@gmail.com>
2024-04-15 00:34:15 +0900

    도커추가추가

--- server/config/settings.py
+++ server/config/settings.py
@@ -217,6 +217,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
