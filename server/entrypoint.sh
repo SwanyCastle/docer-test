10bb7faa25ec54bb89ad5d8bf1dc1f64e7ec51a0
SwanyCastle <seunghwan712@gmail.com>
2024-04-15 21:17:26 +0900

    settings.py 수정

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -4,3 +4,4 @@ python manage.py collectstatic
 python manage.py makemigrations
 python manage.py migrate
 python manage.py runserver 0.0.0.0:80
+# exec gunicorn --bind 0.0.0.0:8000 config.wsgi:application
