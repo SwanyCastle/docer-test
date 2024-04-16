5e58ca440174d80f38dd7f214985d5c4e4a2f325
SwanyCastle <seunghwan712@gmail.com>
2024-04-16 10:04:53 +0900

    allowed_hosts 설정

--- server/config/settings.py
+++ server/config/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["ec2-13-124-35-222.ap-northeast-2.compute.amazonaws.com"]
 
 
 # Application definition
